6de0a793e9ea1bb9af4d708f948a73750f6ea111
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-02-08 22:49:56 +0100

    init reduced

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>Snippetbase</main>
+      <main>Snippetbase</main>
     </>
   );
 }
