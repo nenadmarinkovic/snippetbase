ca622cf1ff76e5594ee3a74c04fba6b0fe0f8d61
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-02-10 00:06:03 +0100

    setup init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Container } from "../styles/components/layout";
 
 export default function Home() {
   return (
@@ -12,7 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Snippetbase</main>
+      <Container>
+        <main>Snippetbase</main>
+      </Container>
+     
     </>
   );
 }
---
+++ styles/components/layout.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 1280px;
+`;
