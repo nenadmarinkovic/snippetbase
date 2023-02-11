dc984947c104a0517e581fe0d6d7795a03ec32fa
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-02-11 18:32:44 +0100

    fetch markdown added

--- components/Snippets.tsx
+++ components/Snippets.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Table } from "../styles/components/snippets";
 
-function Snippets() {
+function Snippets({ posts }: any) {
+  console.log(posts);
+
   return (
     <div>
       <Table>
@@ -12,42 +14,14 @@ function Snippets() {
             <th>Description</th>
             <th>Programming language</th>
           </tr>
-          <tr>
-            <td>03.05.2023</td>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>JavaScript</td>
-          </tr>
-          <tr>
-            <td>03.05.2023</td>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>JavaScript</td>
-          </tr>
-          <tr>
-            <td>03.05.2023</td>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Go</td>
-          </tr>
-          <tr>
-            <td>03.05.2023</td>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>Go</td>
-          </tr>
-          <tr>
-            <td>03.05.2023</td>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>PHP</td>
-          </tr>
-          <tr>
-            <td>03.05.2023</td>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>JavaScript</td>
-          </tr>
+          {posts.map((post: any, index: any) => (
+            <tr key={index}>
+              <td>{post.data.date}</td>
+              <td>{post.data.title}</td>
+              <td>{post.data.description}</td>
+              <td>{post.data.category}</td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
+import fs from "fs";
+import matter from "gray-matter";
+import type { NextPage } from "next";
+import path from "path";
 import { Container } from "../styles/components/layout";
+import { postFilePaths, POSTS_PATH } from "../utils/mdx-posts";
 import Header from "@/components/Header";
 import Snippets from "@/components/Snippets";
 
-export default function Home() {
+const Home: NextPage = ({ posts }: any) => {
   return (
     <>
       <Head>
@@ -17,8 +22,25 @@ export default function Home() {
       </Head>
       <Container>
         <Header />
-        <Snippets />
+        <Snippets posts={posts} />
       </Container>
     </>
   );
+};
+
+export async function getStaticProps() {
+  const posts = postFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
+    const { content, data } = matter(source);
+
+    return {
+      content,
+      data,
+      filePath,
+    };
+  });
+
+  return { props: { posts }, revalidate: 10 };
 }
+
+export default Home;
