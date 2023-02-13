5f95436abed1a42bc83a86ebfc362cd9da83724b
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-02-13 22:57:00 +0100

    layout change

--- components/Snippets.tsx
+++ components/Snippets.tsx
@@ -17,6 +17,7 @@ function Snippets({ posts }: any) {
           Programming language
         </div>
       </div>
+
       <div className="flex-table row" role="rowgroup">
         <div className="flex-row first" role="cell">
           15.05.2023
@@ -30,39 +31,6 @@ function Snippets({ posts }: any) {
         <div className="flex-row" role="cell">
           JavaScript
         </div>
-      </div>
-      <div className="flex-table row" role="rowgroup">
-        <div className="flex-row first" role="cell">
-          15.05.2023
-        </div>
-        <div className="flex-row" role="cell">
-          Vancouver to Victoria and Butchart
-        </div>
-        <div className="flex-row" role="cell">
-          Let’s try to change the previous flexbox table to CSS grid layout!
-        </div>
-        <div className="flex-row" role="cell">
-          JavaScript
-        </div>
-      </div>
-
-      <div className="flex-table row">
-        <div className="flex-row first" role="cell">
-          15.05.2023
-        </div>
-        <div className="flex-row">Blue Mountains Tours</div>
-        <div className="flex-row">
-          Let’s try to change the previous flexbox table to CSS grid layout!
-        </div>
-        <div className="flex-row">JavaScript</div>
-      </div>
-      <div className="flex-table row">
-        <div className="flex-row first">15.05.2023</div>
-        <div className="flex-row">Milford Sound Coach & Cruise</div>
-        <div className="flex-row">
-          Let’s try to change the previous flexbox table to CSS grid layout!
-        </div>
-        <div className="flex-row">Go</div>
       </div>
     </div>
   );
