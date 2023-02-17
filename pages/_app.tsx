847c6cfb4e5984a9eddbdc71a7840800e9a43ffb
nenadmarinkovic <nenadmarinkovic@protonmail.com>
2023-02-17 22:20:23 +0100

    sw disabled for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { GlobalStyle } from "../styles/global";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .catch((err) => console.log("Service Worker registration failed: ", err));
-  }, []);
+  // useEffect(() => {
+  //   navigator.serviceWorker
+  //     .register("/sw.js")
+  //     .catch((err) => console.log("Service Worker registration failed: ", err));
+  // }, []);
 
   return (
     <>
